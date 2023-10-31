ea0b84232a963d2393449a3b3d5f328e86211f5b
varvara13k <114297092+varvara13k@users.noreply.github.com>
2023-10-31 20:13:29 +0300

    Update task_5.py

--- lab_4/task_5.py
+++ lab_4/task_5.py
@@ -10,7 +10,7 @@ p_b_given_a= np.array([[0.95, 0.025, 0.025],
      [0.025, 0.95, 0.025],
      [0.025, 0.025, 0.95]])
 
-
+# энтропия
 def entropy(p):
     res = 0
     for i in range(len(p)):
@@ -19,19 +19,21 @@ def entropy(p):
                 res -= p[i][j] * math.log2(p[i][j])
                 
     return res/len(p[0])
-    
+#  вероятности для приемника
 def b_probabilities(p_a, p_b_given_a):
     return [sum(p_a * p_b_given_a.T[i]) for i in range(len(p_a))]
-    
+#  энтропия источника  
 H_a = -sum([p_a[i]*math.log2(p_a[i]) for i in range(len(p_a))])
 
-
 p_b = b_probabilities(p_a, p_b_given_a)
+#  энтропия приемника  
 H_b =  -sum([p_b[i]*math.log2(p_b[i]) for i in range(len(p_b))])
-H_b_given_a = entropy(p_b_given_a)
 
+# условная энтропия   
+H_b_given_a = entropy(p_b_given_a)
+# энтропия объединения
 H_ab = H_a + H_b_given_a
 
-
+# Количество информации
 info = H_a + H_b - H_ab
 print(f"Количество информации: {round(info, 3)} бит")
